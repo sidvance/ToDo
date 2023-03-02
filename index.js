4c1ed118b09b763c0a928b2b1b214ab6b197d6fc
Cassidy <cassidy.vance@hotmail.com>
2023-03-01 16:31:50 -0800

    Started assigning tasks

---
+++ index.js
@@ -0,0 +1,9 @@
+let task1 = "Make the bed"
+let task2 = "Go to the gym"
+let task3 = "Do laundry"
+
+if (task2 === "Make the bed"){
+    console.log("Completed!!!! Yay!!!!!")
+}else{
+    console.log("Honey, you in danger girl")
+}
