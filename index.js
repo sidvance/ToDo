03a281b95cfab1b056ffb59fce52fd8f2392870c
Cassidy <cassidy.vance@hotmail.com>
2023-03-02 16:31:45 -0800

    implemented for loop

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ if (task2 === "Make the bed"){
 }else{
     console.log("Honey, you in danger girl")
 }
+
+let tasks = [task1, task2, task3]
+
+//Write a for loop that will print each of the tasks
+//in the task array.
+//Implement the above if statement into that for loop,
+//but only one console.log for each task.
+
+for (let i = 0; i < tasks.length; i++){
+    let string = tasks[i]
+
+    if(tasks[i] === 'Make thhe bed'){
+        string += ": Completed!!! Yay!!!!"
+    }else{
+        string += ": Honey, you in danger girl"
+    }
+
+    console.log(string)
+}
